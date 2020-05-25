282514f566de8e6a5fedd2c085372cfdc8f6e601
Martin Stoffel <martin.adam.stoffel@gmail.com>
2020-05-25 15:51:09 +0100

    impute oar

---
+++ 1_download_imputed_genos.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# download 2020 run
+scp mstoffel@eddie.ecdf.ed.ac.uk:/exports/csce/eddie/biology/groups/pemberton/martin/sheep_imputation/cv_run_full_2020/extract_results/geno_imp/*.txt results/full_1_5_2020/
--- 1_preprocess_snp_genos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# remove 3 individuals from HD chip (they are duplicates or have bad quality)
-plink --bfile ../sheep/data/SNP_chip/Plates_1-2_HD_QC1 --remove ../sheep/data/SNP_chip/ids_to_remove --sheep --make-bed --out ../sheep/data/SNP_chip/Plates_1-2_HD_QC2
-
-# Merge plink HD and LD sheep datasets, keep individual order as in original datasets
-# merge-mode is default so mismatches are set to missing
-# if two variants have the same position, try to merge them (fails if allele names arent matching)
-plink --bfile ../sheep/data/SNP_chip/Plates_1-2_HD_QC2 --bmerge ../sheep/data/SNP_chip/Plates_1to87_QC3.bed ../sheep/data/SNP_chip/Plates_1to87_QC3.bim ../sheep/data/SNP_chip/Plates_1to87_QC3.fam --make-bed --indiv-sort 0 --sheep --merge-equal-pos --out ../sheep/data/SNP_chip/merged_sheep_geno
